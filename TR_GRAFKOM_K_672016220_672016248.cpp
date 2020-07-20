f93b3bdae8661399a65d59ba70ca4ce2bc663bad
672016220 <67912720+672016220@users.noreply.github.com>
2020-07-21 02:47:37 +0700

    Awalan,fungsi2 utama

---
+++ TR_GRAFKOM_K_672016220_672016248.cpp
@@ -0,0 +1,47 @@
+#include<windows.h>
+#include <gl/glut.h>
+#include<math.h>
+
+void init(void);
+void display(void);
+void mouse(int button, int state, int x, int y);
+void keyboard(unsigned char, int, int);
+void ukuran(int, int);
+void mouseMotion(int x, int y);
+
+float xrot = 0.0f;
+float yrot = 0.0f;
+float xdiff = 0.0f;
+float ydiff = 0.0f;
+bool mouseDown = false;
+int is_depth;
+int sudut=0, o=0, p=0, q=0;
+int i=0; int j=0;
+
+int main(int argc, char**argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowSize(800,600);
+    glutInitWindowPosition(250, 80);
+    glutCreateWindow("672016220 - 672016248");
+    init();
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+    glutMouseFunc(mouse);
+    glutMotionFunc(mouseMotion);
+    glutReshapeFunc(ukuran);
+    glutMainLoop();
+    return 0;
+}
+
+void init(void)
+{
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    glEnable(GL_DEPTH_TEST);
+    is_depth = 1;
+    glMatrixMode(GL_MODELVIEW);
+    glPointSize(9.0);
+    glLineWidth(1.5f);
+}
